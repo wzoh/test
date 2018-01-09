3ee2a65f7c0aef5e38ce005c0d3c2dd6a4e08643
wzoh <qichang1993@outlook.com>
2018-01-09 17:16:49 +0800

    修改master文件

--- master.php
+++ master.php
@@ -1,10 +1,12 @@
 <?php
 
+    echo "master45645456";
     echo "master1234567";
     echo "master123123";
     echo "1123123asdasda";
     echo "master";
     echo "master1";
     echo "master2";
+    echo "master3";
 
 ?>
